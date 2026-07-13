30ad84ac6198463bf3f50ecf591028afcff94b35
Larabeatriz08 <laradesouzalima08@gmail.com>
2026-07-13 10:55:19 -0300

    feat: add font sources and update styles; remove unused App.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import "@fontsource/inter";
+import "@fontsource/jetbrains-mono";
+
+import "./index.css";
+
+import App from "./App";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);  
